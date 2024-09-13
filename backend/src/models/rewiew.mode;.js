646142ecd4fe298676c9cc1d6cc9556cc83846b2
Parthik Diyora <ddiyora503@gmail.com>
2024-09-13 06:22:37 +0530

    review shema created

---
+++ backend/src/models/rewiew.mode;.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const reviewSchema = new mongoose.Schema({
+    comment: String,
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    },
+    author:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+    }
+});
+
+module.exports = mongoose.model("Review", reviewSchema);
