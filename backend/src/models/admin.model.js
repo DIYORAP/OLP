04abd318fff5667b78e28f6282f6173a85a5017e
Parthik Diyora <ddiyora503@gmail.com>
2024-09-21 15:07:10 +0530

    cahnge admin schema

--- backend/src/models/admin.model.js
+++ backend/src/models/admin.model.js
@@ -21,35 +21,7 @@ const adminSchema =  new mongoose.Schema({
 }) 
 
 
-// adminSchema.pre("save", async function (next) {
-//     if(!this.isModified("password")) return next(); 
-//       this.password = await bcrypt.hash(this.password, 10)
-//     next()
-// })
 
-// adminSchema.methods.isPasswordCorrect = async function (password){
-//     return await bcrypt.compare(password, this.password)
-// }
-
-// adminSchema.methods.generateAccessToken = function(){
-//     return jwt.sign({
-//         _id:this._id,
-//         Email:this.Email,
-//     },
-//     process.env.ACCESS_TOKEN_SECRET,{
-//         expiresIn:process.env.ACCESS_TOKEN_EXPIRY
-//     })
-// }
-
-// adminSchema.methods.generateRefreshToken = function(){
-//     return jwt.sign({
-//         _id:this._id,
-//         Email:this.Email,
-//     },
-//     process.env.REFRESH_TOKEN_SECRET,{
-//         expiresIn:process.env.REFRESH_TOKEN_EXPIRY
-//     })
-// }
 
 const admin = mongoose.model("admin",adminSchema);
 
