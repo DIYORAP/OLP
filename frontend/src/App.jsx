5a8a3054e095f72ffc13fc353663912c41e417e5
Parthik Diyora <ddiyora503@gmail.com>
2024-10-06 13:14:42 +0530

    sudebarchanges

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Test from './pages/test';
 import Sidebar from './pages/sidebar/Sidebar';
+import Search from './components/Search';
 
 function App() {
   return (
-
     <BrowserRouter>
       <div className="flex flex-col min-h-screen">
         <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path='/test' element={<Test />} />
-            <Route path='/side' element={<Sidebar />} />
-          </Routes>
+        <main className="flex-grow flex">
+          <Sidebar /> {/* Sidebar remains fixed for routes that need it */}
+          <div className="flex-grow">
+            <Routes>
+              <Route path="/test" element={<Test />} />
+              <Route path="/search" element={<Search />} />
+            </Routes>
+          </div>
         </main>
         <Footer />
       </div>
     </BrowserRouter>
-
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/pages/sidebar/Sidebar.jsx
+++ frontend/src/pages/sidebar/Sidebar.jsx
@@ -6,8 +6,8 @@ function Sidebar() {
 
         <div className="flex h-screen">
 
-            <div className="hidden md:flex  z-50 w-56 border-r-2 border-gray-300 bg-white shadow-sm">
-                <div className="flex flex-col overflow-y-auto p-6">
+            <div className="hidden md:flex  z-50  border-r-2 border-gray-300 bg-white shadow-sm">
+                <div className="flex flex-col overflow-y-auto w-56">
                     <Sidebarroute />
                 </div>
             </div>
