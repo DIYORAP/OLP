7cdeb15e902ad13fa79e07cbbb782d737bafcebf
Parthik Diyora <ddiyora503@gmail.com>
2024-10-12 13:28:48 +0530

    section Schema define

---
+++ backend/model/Section.model.js
@@ -0,0 +1,17 @@
+import mongoose, { model } from "mongoose";
+
+const sectionSchema=new mongoose.Schema({
+    sectionName:{
+        type:String,
+
+    },
+    subSection:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            required:true,
+            ref:"SubSection",
+        },
+    ],
+});
+
+module.exports=mongoose.model("Section",sectionSchema);
