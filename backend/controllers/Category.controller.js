87b0825651a3caf9c79027a0bd040cf8b02aac95
Parthik Diyora <ddiyora503@gmail.com>
2024-10-13 16:01:23 +0530

    category addcourse to category

--- backend/controllers/Category.controller.js
+++ backend/controllers/Category.controller.js
@@ -29,4 +29,61 @@ export const createCategory=async(req,res,next)=>{
     }
 }
 
+export const showAllCategories=async(req,res,next)=>{
+    try{
+        const allCategorys=await Category.find({},
+            {name:true,description:true}
+        );
+
+        res.status(200).json({success:true,data:allCategorys});
+
+    }
+ catch(error){
+    return res.status(500).json({
+        success:false,
+        message:error.message,
+    });
+  }
+};
+
+
+export const addCourseToCategory=async(req,res)=>{
+    const {courseId,categoryId}=req.body;
+       try {
+             const category=await Category.findById(categoryId);
+             if(!category){
+                return res.status(404).json({
+                    success:false,
+                    message:"Category not Found",
+                });
+             }
+
+             const course=await Course.findById(courseId);
+             if(!course){
+                return res.status(404).json({
+                    success:false,
+                    message:"Course Not Found",
+                });
+             }
+
+             if(category.courses.includes(courseId)){
+                return res.status(200).json({
+                    success: true,
+                    message: "Course already exists in the category",
+                });
+             }
+               category.courses.push(courseId);
+		       await category.save();
+		       return res.status(200).json({
+			    success: true,
+			    message: "Course added to category successfully",
+		});
+       } catch (error) {
+        return res.status(500).json({
+			success: false,
+			message: "Internal server error",
+			error: error.message,
+		});
+       }
+}
 
