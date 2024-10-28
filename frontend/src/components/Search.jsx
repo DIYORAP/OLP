fdde5a17caf61ba2466ae60653bbc5e798b7400c
Parthik Diyora <ddiyora503@gmail.com>
2024-10-28 20:40:09 +0530

    nmm

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Search() {
     return (
         <div>
-            this is seardbsbvvhawiuegqw;oiheioqwjeiobhqwjevhgwtugqwehpiqwhgegqwkuuhiojpijuoch pagemdbhsvhjbdjsa dj,as dm,n shdbashd ,mas dashbxbas dsbdjhb
-        </div>
+
+            EE        </div>
     )
 }
