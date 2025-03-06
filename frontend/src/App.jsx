716c2567493b7553f3b8db9c7f8d548f61b9addd
Parthik Diyora <ddiyora503@gmail.com>
2025-03-06 19:05:30 +0530

    some private protedcted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,23 +5,18 @@ import Footer from './components/Footer';
 import Search from './components/Search';
 import Home from './pages/Home';
 import SidebarLayout from './components/insructor/SidebarLayout';
-import Mess from './components/Mess';
 import Courses from './pages/Courses';
 import StudSidebarLayout from './components/student/studSidebarLayout';
 import Purchases from './pages/student/EnrolledCourses';
 import Session from './pages/student/Session';
 import Setting from './pages/student/Setting';
-//import InsruCourses from './pages/instuctor/InsruCourses';
-//simport CreatePage from './pages/instuctor/CreatePage';
 import Toasterprovider from './components/providers/toaster-provider';
 import SignUp from './pages/signup/SignUp';
 import Login from './pages/login/Login';
-//import InsruSignUp from './pages/signup/InsruSignUp';
 import PrivateRoute from './components/PrivateRoute';
 import Profile from './pages/Profile';
 import AddCourse from './components/insructor/Dashboard/AddCourses';
 import "./App.css";
-//import InstuLogin from './pages/login/InstuLogin';
 import HeroHeader from './pages/instuctor/InsruHero';
 import MyCourses from './components/insructor/Dashboard/Courses/MyCourses';
 import EditCourse from './components/insructor/Dashboard/EditCourse/EditCourse';
@@ -33,10 +28,16 @@ import ViewCourse from './pages/ViewCourse';
 import InstructorSession from './pages/instuctor/Session';
 import StudentSession from './pages/student/Session';
 import VideoCallPage from './pages/Metting';
-
+import { useSelector } from 'react-redux';
+import LoadingBar from 'react-top-loading-bar';
+import { ACCOUNT_TYPE } from './utils/constants';
 function App() {
+  const user = useSelector((state) => state.user?.currentUser?.role);
+  ;
+  console.log(user)
   return (
     <>
+
       <BrowserRouter>
         <Header />
 
@@ -50,10 +51,10 @@ function App() {
 
           {/* <Route path="/instructor/signup" element={<InsruSignUp />} /> */}
           {/* <Route path="/instructor/login" element={<InstuLogin />} /> */}
-          <Route path="/courses/:courseId" element={<CourseDetails />} />
-
-
-          <Route path="/session/:sessionId" element={<VideoCallPage />} />
+          <Route element={<PrivateRoute />}>
+            <Route path="/courses/:courseId" element={<CourseDetails />} />
+            <Route path="/session/:sessionId" element={<VideoCallPage />} />
+          </Route>
           <Route element={<PrivateRoute />}>
             <Route path='/settings' element={<Settings />} />
 
@@ -61,48 +62,61 @@ function App() {
           </Route>
 
 
+          {user === ACCOUNT_TYPE.INSTRUCTOR && (
 
-          <Route element={<SidebarLayout />}>
-            <Route path="/dashboard/mycourses" element={<MyCourses />} />
-            <Route path="/dashboard/addcourse" element={<AddCourse />} />
+            <Route element={<PrivateRoute />}>
+
+              <Route element={<SidebarLayout />}>
+                <Route path="/dashboard/mycourses" element={<MyCourses />} />
+                <Route path="/dashboard/addcourse" element={<AddCourse />} />
+                <Route
+                  path="dashboard/edit-course/:courseId"
+                  element={<EditCourse />}
+
+                />
+                <Route path="/insructor/session" element={<InstructorSession />} />
+
+              </Route>
+            </Route>
+          )}
+
+          <Route element={<PrivateRoute />}>
             <Route
-              path="dashboard/edit-course/:courseId"
-              element={<EditCourse />}
+              element={
 
-            />
-            <Route path="/insructor/session" element={<InstructorSession />} />
+                <ViewCourse />
 
-          </Route>
+              }
+            >
 
-          <Route
-            element={
-
-              <ViewCourse />
-
-            }
-          >
-
-            <>
-              <Route
-                path="/dashboard/enrolled-courses/view-course/:courseId/section/:sectionId/sub-section/:subsectionId"
-                element={<VideoDetails />}
-              />
-            </>
+              <>
+                <Route
+                  path="/dashboard/enrolled-courses/view-course/:courseId/section/:sectionId/sub-section/:subsectionId"
+                  element={<VideoDetails />}
+                />
+              </>
+            </Route>
 
           </Route>
-          <Route element={<StudSidebarLayout />}>
-            <Route path="/student/courses" element={<Courses />} />
-            <Route
-              path="/dashboard/enrolled-courses"
-              element={<EnrolledCourses />}
-            />
-            <Route path="/student/session" element={<StudentSession />} />
-            <Route path="/student/setting" element={<Setting />} />
+          {user === ACCOUNT_TYPE.STUDENT && (
+
+            <Route element={<PrivateRoute />}>
+
+              <Route element={<StudSidebarLayout />}>
+                <Route path="/student/courses" element={<Courses />} />
+                <Route
+                  path="/dashboard/enrolled-courses"
+                  element={<EnrolledCourses />}
+                />
+                <Route path="/student/session" element={<StudentSession />} />
+                <Route path="/student/setting" element={<Setting />} />
 
 
+              </Route>
+            </Route>
+          )}
 
-          </Route>
-
+          <Route path="*" element={<Home />} />
 
         </Routes>
 
