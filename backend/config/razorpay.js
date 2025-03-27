faa768fce15e114cd5941d233c5a2aaee68c534d
Parthik Diyora <ddiyora503@gmail.com>
2025-03-27 14:59:51 +0530

    versel chnges

--- backend/config/razorpay.js
+++ backend/config/razorpay.js
@@ -8,5 +8,5 @@ export const instance = new Razorpay({
     key_secret: process.env.RAZORPAY_SECRET,
 });
 
-console.log("Razorpay Key ID:", process.env.RAZORPAY_KEY);
-console.log("Razorpay Secret:", process.env.RAZORPAY_SECRET);
+// console.log("Razorpay Key ID:", process.env.RAZORPAY_KEY);
+// console.log("Razorpay Secret:", process.env.RAZORPAY_SECRET);
